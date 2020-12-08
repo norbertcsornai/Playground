2b2455d65a8eb582923ad0e28856beec5f041264
norbertcsornai <58106271+norbertcsornai@users.noreply.github.com>
2020-12-08 13:22:01 +0200

    StealthGame

---
+++ UnrealEngineGames/StealthGame/Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+#include "C:/Program Files/Epic Games/UE_4.25/Engine/Source/Runtime/Engine/Classes/Components/BoxComponent.h"
+#include "C:/Program Files/Epic Games/UE_4.25/Engine/Source/Runtime/Engine/Classes/Components/DecalComponent.h"
+#include "FPSCharacter.h"
+#include "FPSGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.0f));
+
+	RootComponent = OverlapComp;
+
+	OverlapComp->SetHiddenInGame(false);
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->DecalSize = FVector(200.0f, 200.0f, 200.0f);
+	DecalComp->SetupAttachment(RootComponent);
+}
+
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Log, TEXT("Overlaped with extraction zone"));
+
+	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
+	if (MyPawn == nullptr)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Pawn is null!"));
+		return;
+	}
+	
+	if (MyPawn->bIsCarryingObjective)
+	{
+		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
+		if (GM)
+		{
+			GM->CompleteMission(MyPawn, true);
+		}
+	}
+	else
+	{
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
+	}
+	
+	//UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone."));
+}
+
